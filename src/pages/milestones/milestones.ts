b66f0afc18d5c9b91c62a24e9a74afdb7194d0b7
Sha Alibhai <shaalibhai@nboxedtechnology.com>
2016-12-06 11:40:28 -0500

    edit milestone

--- src/pages/milestones/milestones.ts
+++ src/pages/milestones/milestones.ts
@@ -69,6 +69,23 @@ export class MilestonesPage {
     this.taskFactory.update();
   }
 
+  edit(milestone: Task, slidingItem: ItemSliding): void {
+    let milestoneModal = this.modalCtrl.create(TaskModal, {
+      action: 'Edit',
+      type: 'Milestone',
+      task: milestone
+    });
+
+    milestoneModal.onDidDismiss(milestone => {
+      if (milestone) {
+        this.taskFactory.update();
+      }
+    });
+
+    slidingItem.close();
+    milestoneModal.present();
+  }
+
   delete(index: number, slidingItem: ItemSliding): void {
     this.taskFactory.removeMilestone(this.resolution, index).then(() => {
       slidingItem.close();
