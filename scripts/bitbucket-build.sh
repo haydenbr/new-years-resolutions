600ea731177ba5d8df0643395d54578515a9dee6
Hayden Braxton <hayden.braxton.dev@gmail.com>
2018-02-03 12:59:13 -0500

    fix(pipelines): update pipelines with new names for build env scripts and build command

--- scripts/bitbucket-build.sh
+++ scripts/bitbucket-build.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 cp -r ../../../../../app/node_modules ./node_modules
-cp -r ../../../../../app/platforms ./platforms
-cp -r ../../../../../app/plugins ./plugins
 
 echo Profile is $PROFILE $FTP_USERNAME
-npm run build-browser
-# sh build-scripts/deploy.sh
+npm run build:browser
