af1de41e5d93dce318e4f80a7fa0d89648d63068
Hayden Braxton <hayden.braxton.dev@gmail.com>
2018-03-03 06:50:34 +0530

    correct script and add empty ignore

--- scripts/docker-run.sh
+++ scripts/docker-run.sh
@@ -2,11 +2,11 @@ docker run -it \
 	-p 8100:8100 \
 	-p 35729:35729 \
 	-p 53703:53703 \
-	- $(pwd)/src:/opt/app/src \
-	- $(pwd)/webpack:/opt/app/webpack \
-	- $(pwd)/tslint.json:/opt/app/tslint.json \
-	- $(pwd)/tsconfig.json:/opt/app/tsconfig.json \
-	- $(pwd)/package.json:/opt/app/package.json \
-	- $(pwd)/app.config.js:/opt/app/app.config.js \
-	- $(pwd)/ionic.config.json:/opt/app/ionic.config.json \
+	-v $(pwd)/src:/opt/app/src \
+	-v $(pwd)/webpack:/opt/app/webpack \
+	-v $(pwd)/tslint.json:/opt/app/tslint.json \
+	-v $(pwd)/tsconfig.json:/opt/app/tsconfig.json \
+	-v $(pwd)/package.json:/opt/app/package.json \
+	-v $(pwd)/app.config.js:/opt/app/app.config.js \
+	-v $(pwd)/ionic.config.json:/opt/app/ionic.config.json \
 	unboxedtechnology/new-years-resolutions
